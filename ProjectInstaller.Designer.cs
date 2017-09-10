fcc6f276991f3799068a3d2e6238e094168b20e9
Harshit Yadav <harshityadav95@gmail.com>
2017-09-10 15:31:37 +0530

    Auto start set to true

--- ProjectInstaller.Designer.cs
+++ ProjectInstaller.Designer.cs
@@ -39,6 +39,7 @@
             // 
             // serviceInstaller1
             // 
+            this.serviceInstaller1.DelayedAutoStart = true;
             this.serviceInstaller1.ServiceName = "Service1Harshit";
             this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
             // 
