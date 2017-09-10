10dea18e36fe52e2f0833985846e1dcbf7f5804f
Harshit Yadav <harshityadav95@gmail.com>
2017-09-10 13:45:38 +0530

    Service created

--- Service1.cs
+++ Service1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+
 
 namespace myservicelearn
 {
     public partial class Service1 : ServiceBase
     {
+        private Timer timer1 = null;
+
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +23,22 @@ namespace myservicelearn
 
         protected override void OnStart(string[] args)
         {
+            timer1 = new Timer();
+            this.timer1.Interval = 30000; //every 30 secs
+            this.timer1.Elapsed += new System.Timers.ElapsedEventHandler(this.timer1_Tick);
+            timer1.Enabled = true;
+            Library.WriteErrorLog("Test window service started");
+        }
+        private void timer1_Tick(object sender, ElapsedEventArgs e)
+        {
+            //Write code here to do some job depends on your requirement
+            Library.WriteErrorLog("Timer ticked and some job has been done successfully");
         }
 
         protected override void OnStop()
         {
+            timer1.Enabled = false;
+            Library.WriteErrorLog("Test window service stopped");
         }
     }
 }
